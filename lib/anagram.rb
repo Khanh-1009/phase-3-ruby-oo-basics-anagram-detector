62bdbaf4664db0307ff511f43dd14cf7d36a3a79
Khanh Tran <katetrannbkhanh@gmail.com>
2023-03-01 12:42:49 -0800

    commit tree ec05c7fc444a36b6ddbc4197f2ca7f2f7b9a4ca4

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match=(match)
+        @match = match
+    end
+end
+
+word_given = Anagram.new(word)
+word_given.match(%w(match))
